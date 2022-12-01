f4ff6552e675dcafe5fb86f9bad939bca9155187
sanam1060 <sanam_g1990@yahoo.com>
2022-12-01 18:12:00 -0500

    test heroku

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
-app.use(express.static("./public"));
+app.use(express.static("public"));
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
@@ -63,7 +63,7 @@ function deleteNote(id, notesArray){
 
 // GET /notes should return the notes.html file
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/notes.html"));
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
 // GET note title query
@@ -76,27 +76,27 @@ app.get("/api/notes", (req, res) => {
 });
 
 // GET id query
-app.get("api/notes/:id", (req, res) => {
+app.get("/api/notes/:id", (req, res) => {
   let results = findById(req.params.id, notes);
   res.json(results);
 });
 
 // Create note
-app.post("api/notes", (req, res) => {
+app.post("/api/notes", (req, res) => {
   req.body.id = notes.length.toString();
   const note = createNote(req.body, notes);
   res.json(note);
 });
 
 // Delete note
-app.post('api/notes', (req, res) => {
+app.delete('/api/notes/:id', (req, res) => {
   let results = deleteNote(req.params.id, notes);
   res.json(results);
 });
 
 // GET /* should return the index.html file
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.listen(PORT, () => {
